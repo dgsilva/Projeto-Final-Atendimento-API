bfb9184f5163ba9cba5b59a84ddf127dbe89d60c
dgsilva <diegorj93@gmail.com>
2022-12-03 13:49:35 -0300

    Criação dos atributos da classe Atendimento

--- src/main/java/com/projetofinal/api/domain/models/Atendimento.java
+++ src/main/java/com/projetofinal/api/domain/models/Atendimento.java
@@ -1,5 +1,24 @@
 package com.projetofinal.api.domain.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_atendimento")
 public class Atendimento {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idAtendimento;
+	private String data;
+	private String hora;
+	private String assunto;
+	private String observacoes;
+	private String emailCliente;
 }
