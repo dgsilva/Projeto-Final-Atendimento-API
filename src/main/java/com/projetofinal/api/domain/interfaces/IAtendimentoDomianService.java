6b53ddb38e79c05d35387533d4aba8b52e39f62e
dgsilva <diegorj93@gmail.com>
2022-12-11 22:34:29 -0300

    Implementado o cadastro de atendimentos

---
+++ src/main/java/com/projetofinal/api/domain/interfaces/IAtendimentoDomianService.java
@@ -0,0 +1,8 @@
+package com.projetofinal.api.domain.interfaces;
+
+import com.projetofinal.api.domain.models.Atendimento;
+
+public interface IAtendimentoDomianService {
+
+	Atendimento save(Atendimento atendimento);
+}
---
+++ src/main/java/com/projetofinal/api/domain/services/AtendimentoService.java
@@ -0,0 +1,22 @@
+package com.projetofinal.api.domain.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetofinal.api.domain.interfaces.IAtendimentoDomianService;
+import com.projetofinal.api.domain.models.Atendimento;
+import com.projetofinal.api.infrastructure.repositories.AtendimentoRepository;
+
+@Service
+public class AtendimentoService implements IAtendimentoDomianService {
+
+	@Autowired
+	private AtendimentoRepository atendimentoRepository;
+	
+	@Override
+	public Atendimento save(Atendimento atendimento) {
+		return atendimentoRepository.save(atendimento);
+	}
+
+	
+}
